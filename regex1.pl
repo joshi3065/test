d2eca574e8a59a47ce3adb130336d7abf5cd0bee
root <root@ubuntu.aa.com>
2017-04-01 13:15:58 +0300

    First Script

---
+++ regex1.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+$line = "Host: 10.0.0.0 (jos.lss.emc.com) 22/port";
+
+#($ip) = $line =~m/(.*) \(/;
+#($dns) = $line =~m/\((.*)\)/;
+($ip,$dns) = $line =~m/Host: (.*) \((.*)\)/;
+
+print "1 $ip\n";
+print "2 $dns\n";
